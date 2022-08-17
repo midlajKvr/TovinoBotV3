77ced067c7f453054b6abe68827f8f0f38418337
midlajKvr <108023292+midlajKvr@users.noreply.github.com>
2022-08-17 12:01:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -719,16 +719,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-        elif query.data == "video":
-        buttons = [[
-            InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='song')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.VIDEO_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
     elif query.data == "ban":
         buttons = [[
             InlineKeyboardButton('𝖡𝖺𝖼𝗄', callback_data='help')
